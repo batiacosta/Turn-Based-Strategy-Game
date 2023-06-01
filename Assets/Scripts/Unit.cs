a0ff40a4633bcb3fdfc841ccd503550ac46a0172
David Leonardo Acosta Laverde <david.acosta.ing@gmail.com>
2023-06-01 18:15:29 -0500

    Move Unit

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    private Vector3 _targetPosition;
+    private void Update()
+    {
+        Vector3 position = transform.position;
+        var deltaPositionTarget = Vector3.Distance(position, _targetPosition);
+        var stoppingDistance = 0.1f;
+        if (deltaPositionTarget > stoppingDistance)
+        {
+            Vector3 moveDirection = (_targetPosition - position).normalized;
+            float moveSpeed = 4f;
+            transform.position += moveDirection * moveSpeed * Time.deltaTime;
+        }
+        
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Move(new Vector3(4, 0, 4));
+        }
+    }
+
+    private void Move(Vector3 targetPosition)
+    {
+        var delta = Vector3.Distance(transform.position, _targetPosition);
+        if (delta < 0.5f)
+        {
+            
+        }
+        _targetPosition = targetPosition;
+    }
+}
