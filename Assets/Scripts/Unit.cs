a97e8182c9d4be0c46f81c9c472cf79cf065c4b3
David Leonardo Acosta Laverde <david.acosta.ing@gmail.com>
2023-06-02 11:07:41 -0500

    Unit animation initial setup

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -3,10 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Unit : MonoBehaviour
 {
+    [SerializeField] private Animator unitAnimator;
     private Vector3 _targetPosition;
+    private static readonly int IsWalking = Animator.StringToHash("IsWalking");
+
     private void Update()
     {
         Vector3 position = transform.position;
@@ -18,6 +22,11 @@ public class Unit : MonoBehaviour
             Vector3 moveDirection = (_targetPosition - position).normalized;
             float moveSpeed = 4f;
             transform.position += moveDirection * moveSpeed * Time.deltaTime;
+            unitAnimator.SetBool(IsWalking, true);
+        }
+        else
+        {
+            unitAnimator.SetBool(IsWalking, false);
         }
         
         if (Input.GetMouseButtonDown(0))
