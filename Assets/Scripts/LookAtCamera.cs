4ef711ac089aaa25844765e64364a7e967373ab1
David Leonardo Acosta Laverde <david.acosta.ing@gmail.com>
2023-06-08 12:04:09 -0500

    Unit UI setup
    Health bar
    Actions remaining per unit

---
+++ Assets/Scripts/LookAtCamera.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour
+{
+    [SerializeField] private bool invert;
+    private Transform _cameraTransform;
+
+    private void Awake()
+    {
+        _cameraTransform = Camera.main.transform;
+    }
+
+    private void LateUpdate()
+    {
+        if (invert)
+        {
+            Vector3 directionToCamera = (_cameraTransform.position - transform.position).normalized;
+            transform.LookAt(transform.position + directionToCamera * -1);
+        }
+        else
+        {
+            transform.LookAt(_cameraTransform);
+        }
+        
+        
+    }
+}
